1db9e19b1a7d298cca4140c634b39bc55e441581
Leonardo Palinkas <palinkas@alunos.utfpr.edu.br>
2019-10-24 10:51:30 -0300

    Initial Commit

---
+++ GetReady/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetReady
+//
+//  Created by leonardo palinkas on 24/10/19.
+//  Copyright © 2019 leonardo palinkas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
