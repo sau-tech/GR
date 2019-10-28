9658b5b760588657a4204ca75050193bc0dd13a5
Leonardo Palinkas <palinkas@alunos.utfpr.edu.br>
2019-10-28 09:16:25 -0300

    segundo commit

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -1,20 +1,54 @@
-//
-//  ViewController.swift
-//  GetReady
-//
-//  Created by leonardo palinkas on 24/10/19.
-//  Copyright © 2019 leonardo palinkas. All rights reserved.
-//
+
 
 import UIKit
+import SpriteKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var BtnCapture: UIButton!
+    @IBOutlet weak var BtnPause: UIButton!
+    @IBOutlet weak var txtWelcome: UILabel!
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var txtRedScore: UILabel!
+    @IBOutlet weak var txtBlueScore: UILabel!
+    @IBOutlet weak var txtTimer: UILabel!
+    @IBOutlet weak var form: UIImageView!
+    @IBOutlet weak var contador: UILabel!
+
+    @IBOutlet weak var prepareToPlayView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        prepareToPlayView.isHidden = false
+        prepareToPlayView.alpha = 1
+        btnPlay.isEnabled = true
+        
+        super.viewWillAppear(animated)
+        
+    }
+    
+    
+    @IBAction func welcome(_ sender: Any) {
+        prepareToPlayView.isHidden = true
+        prepareToPlayView.alpha = 1
+        btnPlay.isEnabled = false
+        form.isHidden = false
+        form.alpha = 1
+    }
+    
 }
+    
+  
+    
+
 
---
+++ Controller/WinViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WinViewController.swift
+//  GetReady
+//
+//  Created by leonardo palinkas on 25/10/19.
+//  Copyright © 2019 leonardo palinkas. All rights reserved.
+//
+
+import UIKit
+
+class WinViewController: UIViewController {
+
+    @IBOutlet weak var BackGround: UIImageView!
+    @IBOutlet weak var reload: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
