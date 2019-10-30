3980711cd5e3746f5b7a5187988afe17eee4f8bf
Saulo <oi@saulo.tech>
2019-10-30 11:14:29 -0300

    Cria timer e pontuação

---
+++ Model/Team.swift
@@ -0,0 +1,16 @@
+//
+//  Times.swift
+//  GetReady
+//
+//  Created by Saulo de Freitas Martins da Silva on 30/10/19.
+//  Copyright © 2019 leonardo palinkas. All rights reserved.
+//
+
+import Foundation
+
+class Team {
+    var points = 0
+    init(pts: Int) {
+        self.points = pts
+    }
+}
