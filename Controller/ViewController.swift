16f70ae5f21c9f7c3d96290ef0b22186107e866d
Dilay <viniciusdilay@protonmail.com>
2019-11-08 17:54:44 -0300

    add faustoKit

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIImagePi
     var character: BodyTrackedEntity?
     let characterOffset: SIMD3<Float> = [0, 0, 0] // Offset the character by one meter to the left
     let characterAnchor = AnchorEntity()
-
     
     // A tracked raycast which is used to place the character accurately
     // in the scene wherever the user taps.
@@ -22,7 +21,9 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIImagePi
     var tapPlacementAnchor: AnchorEntity?
     
     var showRobot = true
-
+    var startTracking = false
+    var bodyPosition1 : PositionCases!
+    
     var model = Model.shared
     var timer = Timer()
     let faustoKit = FaustoKit()
@@ -337,9 +338,30 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIImagePi
     // TODO: PUT THE FRAMEWORK HERE!
     func judgePose() -> Double {
         var score : Double
-        score = Double.random(in: 0.5 ..< 1.0)
-        print(model.secondsInPose)
-        return score
+        let forms = Forms.shared
+//        score = Double.random(in: 0.5 ..< 1.0)
+//        print(model.secondsInPose)
+//        return score
+        if startTracking {
+
+            if forms.Poses[0].leftArmCase == bodyPosition1.leftArmCase &&
+                forms.Poses[0].leftHandCase == bodyPosition1.leftHandCase &&
+                forms.Poses[0].leftKneeCase == bodyPosition1.leftKneeCase &&
+                forms.Poses[0].leftLegCase == bodyPosition1.leftLegCase &&
+                forms.Poses[0].rightArmCase == bodyPosition1.rightArmCase &&
+                forms.Poses[0].rightHandCase == bodyPosition1.rightHandCase &&
+                forms.Poses[0].rightKneeCase == bodyPosition1.rightKneeCase &&
+                forms.Poses[0].rightLegCase == bodyPosition1.rightLegCase {
+                
+                print("certo")
+                return 1
+
+        } else { print("ta errado"); return 0}
+
+        // print(bodyPosition1)
+
+        }
+        return 0
     }
     
     func prize(){
@@ -455,9 +477,9 @@ class ViewController: UIViewController, AVCapturePhotoCaptureDelegate, UIImagePi
                 
             }
             
-            if let character = character {
-            
-                faustoKit.BodyTrackingPosition(character: character, bodyAnchor: bodyAnchor)
+            if let character = character{
+                startTracking = true
+                bodyPosition1 = faustoKit.BodyTrackingPosition(character: character, bodyAnchor: bodyAnchor)
                 
             }
         }
